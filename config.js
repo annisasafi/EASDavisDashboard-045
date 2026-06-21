d9a9299acc2dff9dcafd8f8775b602641303ff27
annnisa safitri <iniannisasafian@gmail.com>
2026-06-21 22:35:21 +0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-let GROQ_API_KEY = typeof window.GROQ_API_KEY !== 'undefined' ? window.GROQ_API_KEY : null;
-
-if (!GROQ_API_KEY) {
-    GROQ_API_KEY = prompt("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");
-}
